00dac7f753e95f271d8ddec5c0a400e1c2f2af8e
xaby <xabynix@gmail.com>
2024-02-07 22:02:17 +0100

    back to originaryname for env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
-/* const meteoApiKey = import.meta.env.VITE_OPEN_WEATHER_KEY as string;
-const geoapifyKey = import.meta.env.VITE_GEOAPIFY_API_KEY as string; */
+const meteoApiKey = process.env.VITE_OPEN_WEATHER_KEY as string;
+const geoapifyKey = process.env.VITE_GEOAPIFY_API_KEY as string;
 
 export const endpoints = {
-	forecast: `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=8e9e127d1c569633e7ee929715115547`, //${meteoApiKey},
-	weather: `https://api.openweathermap.org/data/2.5/weather?appid=8e9e127d1c569633e7ee929715115547&units=metric&lang=it`,
-	autocomplete: `https://api.geoapify.com/v1/geocode/autocomplete?apiKey=e6e42ae38e2e4ba4a22505fcd406d843&lang=it&format=json&type=city`,
+	forecast: `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${meteoApiKey}`,
+	weather: `https://api.openweathermap.org/data/2.5/weather?appid=${meteoApiKey}&units=metric&lang=it`,
+	autocomplete: `https://api.geoapify.com/v1/geocode/autocomplete?apiKey=${geoapifyKey}&lang=it&format=json&type=city`,
 };
